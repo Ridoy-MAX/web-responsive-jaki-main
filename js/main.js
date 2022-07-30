ed99032a798016b5a829f6788ae5eda7ca9cb708
Ridoy-MAX <pou4w98y@gmail.com>
2022-07-30 23:19:28 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// const btn = document.getElementById('btn1');
+// const navbar = document.getElementById('navbar');
+
+// btn.addEventListener('click', function onClick() {
+//     navbar.style.backgroundColor = '#2596be';
+// });
+
+// btn.addEventListener('click', function onClick() {
+//     navbar.style.backgroundColor = '#2596be';
+// });
+
+
+// function changeColor1() {
+//     navbar.style.backgroundColor = '#2596be';
+
+// }
+
+// function changeColor2() {
+//     navbar.style.backgroundColor = '#FFFFFF';
+// }
+
+// Thanks to http://youmightnotneedjquery.com/
+
+// const div = document.getElementById("navbar");
+// const addBtn = document.getElementById("add-btn");
+// const removeBtn = document.getElementById("remove-btn");
+
+// addBtn.addEventListener("click", () => {
+//   div.classList.add("shadow");
+// });
+
+// removeBtn.addEventListener("click", () => {
+//   div.classList.remove("shadow");
+// });
